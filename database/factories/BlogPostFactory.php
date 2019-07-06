f4ac5f8bd034f1574754ad197958f01dae7bd378
jebbyk <zaicev9797@gmail.com>
2019-07-07 00:32:47 +0600

    delete unused files form /laravel folder and added fake content generators for tables

---
+++ database/factories/BlogPostFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+
+$factory->define(\App\Models\BlogPost::class, function (Faker $faker) {
+        //
+
+        $title = $faker->sentence(rand(3,8), true);
+        $txt = $faker->realText(rand(1000,4000));
+        $isPublished = rand(1,5) > 1;
+
+        $createdAt = $faker->dateTimeBetween('-2 months', '-2 days');
+
+        $data = [
+            'category_id' =>rand(1,11),
+            'user_id' => (rand(1,5) == 5) ? 1:2, //sometimes first, but normaly second user
+            'title' => $title,
+            'slug' => str_slug($title),
+            'excerpt' => $faker->text(rand(40,100)),
+            'content_raw' => $txt,
+            'content_html' => $txt,
+            'is_published' => $isPublished,
+            'published_at' => $isPublished ? $faker->dateTimeBetween('-2 months', '-1 days') : null, //get date from 2 months to 1 day older
+            'created_at' => $createdAt,
+            'updated_at' => $createdAt,
+        ];
+
+        return $data;
+});
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+
+        $data = [
+        [
+            
+            'name' => 'unknown author',
+            'email' => 'unknown@author.com',
+            'password' => bcrypt(str_random(16)),
+        ],
+        [
+            'name' => 'Author', 
+            'email' => 'author@gmail.com',
+            'password' => bcrypt('123123'),
+        ],
+    ];
+
+    DB::table('users')->insert($data);
+    }
+}
